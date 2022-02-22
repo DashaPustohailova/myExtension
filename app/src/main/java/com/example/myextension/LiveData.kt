ba40a18261645f82b35f570153499189caf4f33e
DashaPustohailova <dashulya-pustohailova@mail.ru>
2022-02-22 03:32:12 +0300

    test

---
+++ app/src/main/java/com/example/myextension/LiveData.kt
@@ -0,0 +1,8 @@
+package com.example.chucknorris.enviroment.extention
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+fun <T : Any?, L : LiveData<T>> Fragment.observe(liveData: L, body: (T?) -> Unit) =
+    liveData.observe(viewLifecycleOwner, Observer(body))
---
+++ app/src/main/java/com/example/myextension/view.kt
@@ -0,0 +1,6 @@
+package com.example.chucknorris.enviroment.extention
+
+import android.view.View
+
+fun View.gone() { visibility = View.GONE }
+fun View.visible() { visibility = View.VISIBLE }
